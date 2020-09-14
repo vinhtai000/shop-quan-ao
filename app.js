0794dd40d17ef634226537ad04f5d11195c5fafb
Tai Huynh <tai.huynh@idnow.de>
2020-09-14 22:12:05 +0700

    change production db url

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var uri = 'mongodb://localhost:27017/shopping';
-// var uri = "mongodb://vinhtai:gameover@cluster0-shard-00-00.8s0ta.mongodb.net:27017,cluster0-shard-00-01.8s0ta.mongodb.net:27017,cluster0-shard-00-02.8s0ta.mongodb.net:27017/shopping?ssl=true&replicaSet=atlas-2nq7z2-shard-0&authSource=admin&retryWrites=true&w=majority";
+// var uri = 'mongodb://localhost:27017/shopping';
+var uri = "mongodb://vinhtai:gameover@cluster0-shard-00-00.8s0ta.mongodb.net:27017,cluster0-shard-00-01.8s0ta.mongodb.net:27017,cluster0-shard-00-02.8s0ta.mongodb.net:27017/shopping?ssl=true&replicaSet=atlas-2nq7z2-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(uri, { useMongoClient: true });
 
 // uncomment after placing your favicon in /public
